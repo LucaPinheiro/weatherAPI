b52a7f6f8a61ed9e275d9024f0ffced4ae33f428
LucaPinheiro <lucapgomes11@gmail.com>
2023-11-18 21:48:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// app.js
+const apiKey = "ef0b0973b783e0614ac87612ec04344b";
+
+export async function getCoordinates(cityName) {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
+    );
+    const data = await response.json();
+    const { lat, lon } = data[0];
+    return { lat, lon };
+  } catch (error) {
+    console.error("Erro ao obter coordenadas:", error.message);
+    throw error;
+  }
+}
+
+export async function getCurrentConditions(latitude, longitude) {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    );
+    const data = await response.json();
+    const { feels_like, weather } = data;
+    const description = weather[0].description;
+    return { feels_like, description };
+  } catch (error) {
+    console.error("Erro ao obter condições atuais:", error.message);
+    throw error;
+  }
+}
---
+++ main.js
@@ -0,0 +1,40 @@
+// main.js
+import { getCoordinates, getCurrentConditions } from "./app.js";
+
+document.getElementById("consultButton").addEventListener("click", getWeather);
+
+async function getWeather() {
+  const cityInput = document.getElementById("cityInput");
+  const cityName = cityInput.value;
+
+  if (!cityName) {
+    alert("Por favor, digite o nome da cidade.");
+    return;
+  }
+
+  try {
+    const coordinates = await getCoordinates(cityName);
+    const conditions = await getCurrentConditions(
+      coordinates.lat,
+      coordinates.lon
+    );
+    displayResults(coordinates, conditions);
+    document.getElementById("resultContainer").classList.remove("hidden");
+  } catch (error) {
+    console.error("Erro no aplicativo:", error.message);
+  }
+}
+
+function displayResults(coordinates, conditions) {
+  const coordinatesElement = document.getElementById("coordinates");
+  const conditionsElement = document.getElementById("conditions");
+
+  coordinatesElement.textContent = `Coordenadas: Latitude ${coordinates.lat}, Longitude ${coordinates.lon}`;
+
+  const feelsLike =
+    conditions.feels_like !== undefined
+      ? conditions.feels_like
+      : "Dados não disponíveis";
+
+  conditionsElement.textContent = `Condições Atuais: Sensação Térmica ${feelsLike}, Descrição ${conditions.description}`;
+}
